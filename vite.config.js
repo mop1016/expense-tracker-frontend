03cc06dcdaba2c2ca38305fc6d0ed72aaf0bb199
mop1016 <mop1016@gmail.com>
2025-09-22 22:01:17 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react()],
   server: {
     port: 3000,
-    host: '0.0.0.0', // 允許外部訪問
-    strictPort: true, // 如果端口被佔用則失敗而不是自動選擇其他端口
-  },
+    host: true
+  }
 })
 
